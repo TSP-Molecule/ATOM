34f09336dc79a7e4103bbe5b7fca50d75cb0bd0b
Unknown <selarkin@mtu.edu>
2018-02-16 12:38:14 -0500

    Testing Commit
    Does this work? (Sarah)

--- src/ATOM.java
+++ src/ATOM.java
@@ -1,6 +1,6 @@
 public class ATOM {
 
-    public static void main(String[] args) {
-
-    }
+        public static void main(String[] args) {
+            System.out.println("Does this work?");
+        }
 }
