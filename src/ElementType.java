88b8ba8dfabaf78490116c67de6ca5f480141376
Emily Anible <mtanible@mtu.edu>
2018-02-17 10:45:57 -0500

    Element -> ElementType.

--- src/ElementType.java
+++ src/ElementType.java
@@ -1,48 +1,48 @@
 
 /**
- * Element -
+ * ElementType -
  *      @author Emily Anible
- *      Element Object. Each instance of the object contains information about the element,
+ *      ElementType Object. Each instance of the object contains information about the element,
  *      and can be copied to create duplicate "atoms".
  *      The information about real elements is stored and translated elsewhere,
  *      this object only holds the information about each one used.
  *
  *      //TODO: Maybe call this something different. Is a property of Atoms now, so maybe TypeElement?
  */
-public class Element {
+public class ElementType {
 
 
     /**
-     *  Element Attributes
+     *  ElementType Attributes
      *      Each element has various attributes about it.
      *      The object will store a copy of this information accessible
      *      through the getters.
      *
      *      This information should NEVER change during runtime!
      */
-    private final int atomicNumber;
-    private final String symbol;
-    private final String name;
-    private final double atomicMass;
-    private final int atomicRadius;
-    private final double density;
-    private final int cpkColor;
+    private final int       atomicNumber;
+    private final String    symbol;
+    private final String    name;
+    private final double    atomicMass;
+    private final int       atomicRadius;
+    private final double    density;
+    private final int       cpkColor;
 
-    public Element(int atomicNumber,
-                   String symbol,
-                   String name,
-                   double atomicMass,
-                   int atomicRadius,
-                   double density,
-                   int cpkColor) {
+    public ElementType(int      atomicNumber,
+                       String   symbol,
+                       String   name,
+                       double   atomicMass,
+                       int      atomicRadius,
+                       double   density,
+                       int      cpkColor) {
 
-        this.atomicNumber = atomicNumber;
-        this.symbol = symbol;
-        this.name = name;
-        this.atomicMass = atomicMass;
-        this.atomicRadius = atomicRadius;
-        this.density = density;
-        this.cpkColor = cpkColor;
+        this.atomicNumber   = atomicNumber;
+        this.symbol         = symbol;
+        this.name           = name;
+        this.atomicMass     = atomicMass;
+        this.atomicRadius   = atomicRadius;
+        this.density        = density;
+        this.cpkColor       = cpkColor;
     }
 
     public int getAtomicNumber() {
