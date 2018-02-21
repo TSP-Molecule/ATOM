e9a751f2f816d9d82b68972205f5630a427e4a08
Emily Anible <mtanible@mtu.edu>
2018-02-21 17:07:13 -0500

    Elem replaces Element

--- test/AtominomiconTest.java
+++ test/AtominomiconTest.java
@@ -11,34 +11,9 @@ public class AtominomiconTest {
      * @throws IOException
      */
     @Test
-    public void readElements() throws IOException {
+    public void Atominomicon() throws IOException {
         Atominomicon atominomicon = new Atominomicon();
-        String filename = "src/resources/elements.dat";
-        try {
-            atominomicon.readElements(filename);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
-        // Print out result
-//        for (int i = 1; i <= 118; i++) {
-//            System.out.println(atominomicon.getAtominomicon().get(i).toString());
-//        }
-
-        // Try printing out color of a Zinc atom:
-        int zincColor0 = atominomicon.readElem(30).getColor();
-        int zincColor1 = atominomicon.readElem(30, 0).getColor();
-
-        int zincColor2 = atominomicon.readAtom(30).getElement().getColor();
-        int zincColor3 = atominomicon.readAtom(30, 0).getElement().getColor();
-
-        int zincCol  = atominomicon.getColor(30);
-
-        int test = atominomicon.getColor( Elem.Nickel.getNum() );
-
-        System.out.println("Nickel is " + Elem.Nickel.getSymbol());
-
-        System.out.println(Integer.toHexString(test));
-
+        System.out.println( "Atomic Mass: "  + Elem.get(30).getAtomicMass());
     }
 }
