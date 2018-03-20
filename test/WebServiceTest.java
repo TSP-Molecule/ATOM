d795c651eefc92eb55f3aec0823372efa6b6f7a1
Emily Anible <mtanible@mtu.edu>
2018-03-20 19:26:23 -0400

    Fixed WebServiceTest testing API

--- test/WebServiceTest.java
+++ test/WebServiceTest.java
@@ -1,6 +1,5 @@
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import static junit.framework.TestCase.fail;
 
 class WebServiceTest {
 
