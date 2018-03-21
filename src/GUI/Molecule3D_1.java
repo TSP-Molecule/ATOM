b8da2b7cd34629fa659828d302cf9a827f10e6ac
Sarah <selarkin@mtu.edu>
2018-03-21 18:19:05 -0400

    Got the Color working in Enums
    Warning:  messy code will make merge conflict.  please accept my version of enums.Elem.

--- src/GUI/Molecule3D_1.java
+++ src/GUI/Molecule3D_1.java
@@ -22,25 +22,25 @@ public class Molecule3D_1 extends Application {
 
     }
 
-    public Group molecule(Molecule m) {
-        Group molecule = new Group();
-        Atom center = m.getCenter();
-        for (Atom a: m.getAtoms()) {
-
-        }
-        Sphere e1 = new Sphere(100);
-        e1.setMaterial(new PhongMaterial(Color.web(center.getElement().getColor() + "")));
-        for (Bond b: center.getBonds()) {
-            Object [] bond = b.getBondedAtoms().toArray();
-            if (bond[0] == center) {
-
-            }
-            Cylinder c = new Cylinder(50, 100);
-            c.getTransforms().add(new Translate(50, 0, 0 ));
-            c.getTransforms().add(new Rotate(100, 50, 0, 0));
-        }
-        return molecule;
-    }
+//    public Group molecule(Molecule m) {
+//        Group molecule = new Group();
+//        Atom center = m.getCenter();
+//        for (Atom a: m.getAtoms()) {
+//
+//        }
+//        Sphere e1 = new Sphere(100);
+//        e1.setMaterial(new PhongMaterial(Color.web(center.getElement().getColor() + "")));
+////        for (Bond b: center.getBonds()) {
+////            Object [] bond = b.getBondedAtoms().toArray();
+////            if (bond[0] == center) {
+////
+////            }
+////            Cylinder c = new Cylinder(50, 100);
+////            c.getTransforms().add(new Translate(50, 0, 0 ));
+////            c.getTransforms().add(new Rotate(100, 50, 0, 0));
+////        }
+////        return molecule;
+//    }
 
 
 
