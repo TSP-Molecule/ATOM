8339ec1aa1990125d35dd04a7e1697d061de2d18
Emily Anible <mtanible@mtu.edu>
2018-03-27 00:16:54 -0400

    GUI Tweaks

--- src/GUI/GeneralViewer.java
+++ src/GUI/GeneralViewer.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
@@ -20,13 +21,17 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import structures.*;
 import structures.enums.Elem;
+import sun.plugin.javascript.navig.Anchor;
 import web.WebService;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 
 /**
  * Displays the main window of the app with a menu, search bar, and two panels for models and information.
- * @author  Sarah Larkin
+ *
+ * @author Sarah Larkin
  * CS3141, Spring 2018, Team ATOM
  * Date Last Modified: March 25, 2018
  */
@@ -48,55 +53,53 @@ public class GeneralViewer extends Application {
 
     /**
      * Displays the full window with a split view
+     *
      * @return the stage containing the window
      */
     public Stage window() {
         Stage stage = new Stage();
+        stage.setMaxWidth(1000);
+        stage.setMaxHeight(700);
         Group group = new Group();
         GridPane pane = new GridPane();
         pane.setBackground(new Background(new BackgroundFill(Color.rgb(255, 240, 200), new CornerRadii(2), new Insets(2))));
-        pane.setPrefSize(1200, 800);
+        pane.setPrefSize(1000, 700);
 
         TextArea left = new TextArea("Molecule here");
-        left.setMinSize(600, 700);
+        left.setPrefSize(500, 770);
         left.setBackground(new Background(new BackgroundFill(Color.rgb(255, 200, 220), new CornerRadii(2), new Insets(0))));
-        SubScene sub = sub(waterMolecule(), 600, 700);
+        SubScene sub = sub(waterMolecule(), 500, 700);
 
         pane.add(sub, 0, 0);
 
         TextArea right = new TextArea();
-        right.setPromptText("Molecule info will go here");
+        right.setPromptText("Molecule information will be displayed here.");
         right.setEditable(false);
-        right.setMinSize(600, 700);
+        right.setPrefSize(500, 700);
         right.setBackground(new Background(new BackgroundFill(Color.rgb(200, 255, 220), new CornerRadii(2), new Insets(0))));
         textPane.setContent(right);
         textPane.setMinSize(600, 700);
         pane.add(textPane, 1, 0);
 
         BorderPane mainPane = new BorderPane();
-        mainPane.setPrefSize(stage.getWidth(), stage.getHeight());
-        GridPane topGrid = new GridPane();
-        topGrid.add(makeMenuBar(stage), 0, 0);
-        topGrid.add(searchBar(), 1, 0);
 
-        mainPane.setTop(topGrid);
+        //HBox for Menu and Searchbar
+        HBox topBar = new HBox();
+        topBar.getChildren().addAll(makeMenuBar(stage), searchBar());
+
+        mainPane.setTop(topBar);
         mainPane.setCenter(pane);
+
         Scene scene = new Scene(mainPane, 1200, 800);
 
         // Set default selection of subscene upon entry or click
-        sub.setOnMouseEntered(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                sub.requestFocus();
-                subf = true;
-            }
+        sub.setOnMouseEntered(event -> {
+            sub.requestFocus();
+            subf = true;
         });
-        sub.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                sub.requestFocus();
-                oldX = event.getX();
-            }
+        sub.setOnMousePressed(event -> {
+            sub.requestFocus();
+            oldX = event.getX();
         });
         stage.setScene(scene);
         stage.setTitle("ATOM - A Tiny Object Modeler");
@@ -107,12 +110,13 @@ public class GeneralViewer extends Application {
 
     /**
      * Makes a central pane that can be used for organizing panels.
+     *
      * @param stage the window of display
      * @return the grid pane
      */
     private GridPane displayPane(Stage stage) {
         GridPane p = new GridPane();
-       // p.setMinSize(stage.getWidth()/2, stage.getHeight() *2/3);
+        // p.setMinSize(stage.getWidth()/2, stage.getHeight() *2/3);
         p.setBackground(new Background(new BackgroundFill(Color.rgb(255, 0, 0), new CornerRadii(2), new Insets(2))));
         return p;
     }
@@ -120,12 +124,13 @@ public class GeneralViewer extends Application {
 
     /**
      * Makes a subscene which is an alternate option for displaying a 3D model
+     *
      * @param scene the parent scene, or window
-     * @param w the width of the subscene
-     * @param h the height of the subscene
+     * @param w     the width of the subscene
+     * @param h     the height of the subscene
      * @return the subscene
      */
-    private SubScene sub (Parent scene, double w, double h) {
+    private SubScene sub(Parent scene, double w, double h) {
         SubScene scene1 = new SubScene(scene, w, h);
         scene1.setWidth(500);
         scene1.setHeight(600);
@@ -162,20 +167,20 @@ public class GeneralViewer extends Application {
                 } else if (event.getCode() == KeyCode.H) {
                     scene.getTransforms().add(new Rotate(-1, 0, 0, 0, Rotate.Z_AXIS));
                 } else
-                // translate commands
+                    // translate commands
                     if (event.getCode() == KeyCode.W) {
-                    cam.getTransforms().add(new Translate(0, 0, dist));
-                } else if (event.getCode() == KeyCode.S) {
-                    cam.getTransforms().add(new Translate(0, 0, -dist));
-                } else if (event.getCode() == KeyCode.A) {
-                    cam.getTransforms().add(new Translate(-dist, 0, 0));
-                } else if (event.getCode() == KeyCode.D) {
-                    cam.getTransforms().add(new Translate(dist, 0, 0));
-                } else if (event.getCode() == KeyCode.R) {
-                    cam.getTransforms().add(new Translate(0, -dist, 0));
-                } else if (event.getCode() == KeyCode.F) {
-                    cam.getTransforms().add(new Translate(0, dist, 0));
-                }
+                        cam.getTransforms().add(new Translate(0, 0, dist));
+                    } else if (event.getCode() == KeyCode.S) {
+                        cam.getTransforms().add(new Translate(0, 0, -dist));
+                    } else if (event.getCode() == KeyCode.A) {
+                        cam.getTransforms().add(new Translate(-dist, 0, 0));
+                    } else if (event.getCode() == KeyCode.D) {
+                        cam.getTransforms().add(new Translate(dist, 0, 0));
+                    } else if (event.getCode() == KeyCode.R) {
+                        cam.getTransforms().add(new Translate(0, -dist, 0));
+                    } else if (event.getCode() == KeyCode.F) {
+                        cam.getTransforms().add(new Translate(0, dist, 0));
+                    }
                 scene1.requestFocus();
             }
         });
@@ -185,6 +190,7 @@ public class GeneralViewer extends Application {
 
     /**
      * Depicts a hard-coded salt molecule using colors from the element enum.
+     *
      * @return the group of shapes that represent the salt molecule
      */
     public Group saltMolecule() {
@@ -243,7 +249,7 @@ public class GeneralViewer extends Application {
 
         double p = 150;
         ArrayList<Cylinder> bondList = new ArrayList<>();
-        for (int i = 0; i < 12; i++ ) {
+        for (int i = 0; i < 12; i++) {
             Cylinder cy = new Cylinder(10, bond);
             cy.setMaterial(new PhongMaterial(Color.LIGHTGREY));
             bondList.add(cy);
@@ -255,7 +261,7 @@ public class GeneralViewer extends Application {
             cy.setRotationAxis(Rotate.Z_AXIS);
             cy.setRotate(90);
 
-            switch(i) {
+            switch (i) {
                 case 0:
                     cy.setTranslateZ(0);
                     break;
@@ -277,7 +283,7 @@ public class GeneralViewer extends Application {
             Cylinder cy = bondList.get(i);
             cy.setTranslateY(150);
 
-            switch(i) {
+            switch (i) {
                 case 4:
                     cy.setTranslateX(0);
                     cy.setTranslateZ(0);
@@ -303,7 +309,7 @@ public class GeneralViewer extends Application {
             cy.setRotationAxis(Rotate.X_AXIS);
             cy.setRotate(90);
 
-            switch(i) {
+            switch (i) {
                 case 8:
                     cy.setTranslateX(0);
                     cy.setTranslateY(0);
@@ -322,7 +328,7 @@ public class GeneralViewer extends Application {
                     break;
             }
         }
-        for (Cylinder cy: bondList) {
+        for (Cylinder cy : bondList) {
             group.getChildren().add(cy);
         }
 
@@ -333,6 +339,7 @@ public class GeneralViewer extends Application {
 
     /**
      * Depicts a hard-coded water molecule.
+     *
      * @return the group containing the shapes representing the water molecule.
      */
     public Group waterMolecule() {
@@ -354,7 +361,7 @@ public class GeneralViewer extends Application {
         Sphere hydrogen1 = new Sphere(50);
         hydrogen1.setMaterial(whiteMat);
 
-        Cylinder b1 = new Cylinder(10,bond);
+        Cylinder b1 = new Cylinder(10, bond);
         PhongMaterial blackM = new PhongMaterial();
         blackM.setDiffuseColor(Color.BLACK);
         blackM.setSpecularColor(Color.BLACK);
@@ -367,22 +374,22 @@ public class GeneralViewer extends Application {
         oxygen.setTranslateY(0);
         oxygen.setTranslateZ(0);
 
-        hydrogen.setTranslateX(bond * Math.cos(-1 * Math.PI/3));
-        hydrogen.setTranslateY(bond * Math.sin(-1 * Math.PI/3));
+        hydrogen.setTranslateX(bond * Math.cos(-1 * Math.PI / 3));
+        hydrogen.setTranslateY(bond * Math.sin(-1 * Math.PI / 3));
         hydrogen.setTranslateZ(0);
 
-        hydrogen1.setTranslateX(bond * Math.cos(Math.PI/3));
-        hydrogen1.setTranslateY(bond * Math.sin(Math.PI/3));
+        hydrogen1.setTranslateX(bond * Math.cos(Math.PI / 3));
+        hydrogen1.setTranslateY(bond * Math.sin(Math.PI / 3));
         hydrogen1.setTranslateZ(0);
 
-        b1.setTranslateX(bond * Math.cos(-1 * Math.PI/3)/2 - 0);
-        b1.setTranslateY(bond * Math.sin(-1 * Math.PI/3)/2 + 0);
+        b1.setTranslateX(bond * Math.cos(-1 * Math.PI / 3) / 2 - 0);
+        b1.setTranslateY(bond * Math.sin(-1 * Math.PI / 3) / 2 + 0);
         b1.setTranslateZ(0);
         b1.setRotationAxis(Rotate.Z_AXIS);
         b1.setRotate(30);
 
-        b2.setTranslateX(bond * Math.cos(Math.PI/3)/2);
-        b2.setTranslateY(bond * Math.sin(Math.PI/3)/2);
+        b2.setTranslateX(bond * Math.cos(Math.PI / 3) / 2);
+        b2.setTranslateY(bond * Math.sin(Math.PI / 3) / 2);
         b2.setTranslateZ(0);
         b2.setRotationAxis(Rotate.Z_AXIS);
         b2.setRotate(150);
@@ -395,6 +402,7 @@ public class GeneralViewer extends Application {
 
     /**
      * Makes the search bar.
+     *
      * @return the search bar
      */
     private BorderPane searchBar() {
@@ -402,69 +410,76 @@ public class GeneralViewer extends Application {
         TextField search = new TextField();
         search.setPromptText("search");
         search.setEditable(true);
-        search.setPrefSize(800, 40);
+        search.setPrefWidth(390);
         grid.setLeft(search);
 
-
-        Button go = new Button("GO");
-        go.setPrefSize(100, 40);
-        go.setTextFill(Color.GREEN);
-        grid.setRight(go);
-        go.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                String searchText = search.getText();
-                try {
-                    WebService spider = new WebService();
-                    String formula = spider.getFormula(searchText);
-
-                    System.out.println("Input was  : " + searchText);
-                    System.out.println("We got this: " + formula);
-
-                    //If we get a bad input, alert the user and don't proceed.
-                    if (formula == null) {
-                        Alert alert = new Alert(Alert.AlertType.ERROR);
-                        alert.setTitle("Error");
-                        alert.setHeaderText("No result found for \"" + searchText + "\"");
-                        alert.setContentText("We couldn't seem to find a chemical formula that corresponds to the input! Please try again or try a different input.");
-
-                        alert.showAndWait();
-                        return;
-                    }
-
-                    TextArea info = new TextArea();
-                    Molecule mol = new Molecule(formula, searchText);
-
-                    String printout = String.format("%s has the formula %s.\n\n", searchText.substring(0,1).toUpperCase() + searchText.substring(1), formula);
-                    String output = printout + mol;
-
-                    info.setText(output);
-                    info.setEditable(false);
-                    info.setPrefSize(textPane.widthProperty().doubleValue(), textPane.heightProperty().doubleValue());
-                    textPane.setContent(info);
-
-//                    ArrayList<Atom> elemList = new ChemicalFormula(formula).getAtoms();
-//                    String chemicals = String.format("It contains the following elements:\n");
-//                    String atoms = "";
-//                    for (int i = 0; i < elemList.size(); i++) {
-//                        atoms += elemList.get(i).getElement().getName();
-//                        if (i < elemList.size() - 1) {
-//                            atoms += ", ";
-//                        }
-//                        atoms += "\n";
-//                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        search.setOnKeyPressed(ke -> {
+            if (ke.getCode().equals(KeyCode.ENTER)) {
+                searchForMolecule(search.getText());
             }
         });
+
+        Button go = new Button("GO");
+        go.setPrefWidth(100);
+        go.setTextFill(Color.GREEN);
+        grid.setRight(go);
+        go.setOnAction(event -> searchForMolecule(search.getText()));
         return grid;
     }
 
+    /**
+     * Searches for molecule and attempts to update relevant information
+     *
+     * @param searchText User-inputted search text.
+     */
+    private void searchForMolecule(String searchText) {
+        String formula = null;
+        try {
+            formula = WebService.getFormula(searchText);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //If we get a bad input, alert the user and don't proceed.
+        if (formula == null) {
+            giveAlert(Alert.AlertType.ERROR,
+                    "No result found for" + searchText,
+                    "We couldn't seem to find a chemical formula that corresponds to the input! " +
+                            "Please try again or try a different input.");
+            return;
+        }
+
+        TextArea info = new TextArea();
+        Molecule mol = new Molecule(formula, searchText);
+
+        String printout = String.format("%s has the formula %s.\n\n", searchText.substring(0, 1).toUpperCase() + searchText.substring(1), formula);
+        String output = printout + mol;
+
+        info.setText(output);
+        info.setEditable(false);
+        info.setPrefSize(textPane.widthProperty().doubleValue(), textPane.heightProperty().doubleValue());
+        textPane.setContent(info);
+    }
+
+    /**
+     * Creates an Alert and displays it.
+     *
+     * @param alertType    Type that the alert should be
+     * @param header       header of the alert
+     * @param alertMessage message of the alert
+     */
+    private void giveAlert(Alert.AlertType alertType, String header, String alertMessage) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(alertType.toString());
+        alert.setHeaderText(header);
+        alert.setContentText(alertMessage);
+        alert.showAndWait();
+    }
 
 
     /**
      * Creates and returns a pane to display results of a search
+     *
      * @param p
      * @param results
      * @return
@@ -475,7 +490,7 @@ public class GeneralViewer extends Application {
         pane.addRow(0, res);
         pane.addRow(1, new Label("Results will be listed here"));
         pane.addRow(2, new Label("Click a result to view the molecule"));
-        for (Node node: pane.getChildren()) {
+        for (Node node : pane.getChildren()) {
             node.setOnMouseClicked(new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent event) {
@@ -487,13 +502,14 @@ public class GeneralViewer extends Application {
                 }
             });
         }
-       return pane;
+        return pane;
     }
 
 
     /**
      * Makes and returns a menu bar to be displayed in the given stage.
-     * @param p  the stage in which to display the menu
+     *
+     * @param p the stage in which to display the menu
      * @return the new menu bar
      */
     private MenuBar makeMenuBar(Stage p) {
@@ -526,14 +542,15 @@ public class GeneralViewer extends Application {
         MenuItem sources = new MenuItem("Sources");
         help.getItems().addAll(userManual, about, sources);
         bar.getMenus().add(help);
-        bar.setPrefHeight(40);
-        bar.setPrefWidth(300);
+//        bar.setPrefHeight(40);
+        bar.setPrefWidth(500);
         return bar;
     }
 
 
     /**
      * Makes a "file" menu with options to open, save, and exit
+     *
      * @param p the stage in which the menu is displayed
      * @return the new menu
      */
