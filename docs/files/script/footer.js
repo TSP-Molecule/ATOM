34f195da44f786777861b4ffdffb157bf0b6b98a
Crystal Fletcher <sporeluc@gmail.com>
2018-04-04 08:43:08 -0700

    Add default script files
    Java Script default formatting files

---
+++ docs/files/script/footer.js
@@ -0,0 +1,2 @@
+document.getElementsByTagName("main")[0].innerHTML += '<div id="footerBuffer" style="height:60px;"></div>'
+document.write('<div class="footer"> <hr /> <p> &copy; 2018<script>new Date().getFullYear()>2018&&document.write("-"+new Date().getFullYear());</script>, \ TSP-molecule. </p> </div>');
---
+++ docs/files/script/nav.js
@@ -0,0 +1,1 @@
+document.write('<nav>| <a href="/">HOME</a> | <a href="/about/">ABOUT</a> | <a href="/github/">GITHUB</a> |</nav><hr />');
