6f36fac59519aa3b05fd2cd82ee2393a963f3c32
Crystal Fletcher <sporeluc@gmail.com>
2018-04-09 08:12:34 -0700

    Stopping production on the GitHub Pages
    We will just use the md files inside of the project as documentation

--- docs/files/script/footer.js
+++
@@ -1,2 +0,0 @@
-document.getElementsByTagName("main")[0].innerHTML += '<div id="footerBuffer" style="height:60px;"></div>'
-document.write('<div class="footer"> <hr /> <p> &copy; 2018<script>new Date().getFullYear()>2018&&document.write("-"+new Date().getFullYear());</script>, \ TSP-molecule. </p> </div>');
